80bd1c6de85d312b3513a58862c334e85cad979d
eliaszayoud <eng.eliaszayoud@gmail.com>
2024-05-11 01:21:29 +0300

    create superuser

--- storefront/build.sh
+++ storefront/build.sh
@@ -6,5 +6,10 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsuperuser
+
+if [[ $CREATE_SUPERUSER ]];
+then
+  python manage.py createsuperuser --no-input
+fi
+
 # celery -A storefront worker
